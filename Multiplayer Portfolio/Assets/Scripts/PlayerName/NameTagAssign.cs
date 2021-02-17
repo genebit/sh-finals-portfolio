315421e104ca62418c5a8e617eda97390665dab7
genebit <genebitara@gmail.com>
2021-02-17 21:11:43 +0800

    Coding the Save player name
    Added: Material to color-player and changed colors on player, platform

---
+++ Multiplayer Portfolio/Assets/Scripts/PlayerName/NameTagAssign.cs
@@ -0,0 +1,13 @@
+﻿using Photon.Pun;
+using TMPro;
+
+public class NameTagAssign : MonoBehaviourPun
+{
+    private TextMeshProUGUI playernameDisplay;
+
+    void Start()
+    {
+        playernameDisplay = GetComponent<TextMeshProUGUI>();
+        playernameDisplay.text = photonView.Owner.NickName;
+    }
+}
---
+++ Multiplayer Portfolio/Assets/Scripts/PlayerName/SavePlayerName.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Photon.Pun;
+using TMPro;
+
+public class SavePlayerName : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI statusText = null;
+    [SerializeField] private TMP_InputField inputfield = null;
+    
+    public static string playerName;
+    public static bool validName = false;
+    
+    private const string playerPrefsNameKey = "PlayerName";
+
+    private void Start() 
+    {
+        inputfield.text = PlayerPrefs.GetString(playerPrefsNameKey);
+    }
+    public void SaveName()
+    {
+        playerName = inputfield.text;
+
+        if (!string.IsNullOrEmpty(playerName)) 
+        {   
+            validName = true;
+            statusText.text = "Player name saved.";
+            PlayerPrefs.SetString(playerPrefsNameKey, playerName);
+        
+            PhotonNetwork.NickName = playerName;
+        }
+        else 
+        {
+            statusText.text = "Invalid Name. Please enter a new one.";
+        }
+    }
+}
