ac7a0eb39a22553f1703f1c3c841f90d58c33483
genebit <genebitara@gmail.com>
2021-02-17 21:51:24 +0800

    Coded Back Button and downloaded sprites for it

---
+++ Multiplayer Portfolio/Assets/Scripts/Misc/BackButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace Scripts.Misc 
+{
+    public class BackButton : MonoBehaviour
+    {
+        public void ReturnHome() 
+        {
+            // Return to Login
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1);
+        }   
+    }
+}
