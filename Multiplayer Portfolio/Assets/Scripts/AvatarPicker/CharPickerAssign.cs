cd952c593c76360f87037d3b461f8b8196465613
genebit <genebitara@gmail.com>
2021-02-19 20:06:34 +0800

    SEE DESC BELOW.
    Renamed Prefab folder to Resources, Coding the player avatar assign at start, not working need solution asap.
    
    SEND TO BACK ON TODO LIST

--- Multiplayer Portfolio/Assets/Scripts/AvatarPicker/CharPickerAssign.cs
+++ Multiplayer Portfolio/Assets/Scripts/AvatarPicker/CharPickerAssign.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Scripts.AvatarPicker
-{     
-    public class CharPickerAssign : MonoBehaviour
+public class CharPickerAssign : MonoBehaviour
+{
+    public Transform picker;
+    public static string avatarKey;
+
+    public void chooseMale()
     {
-        public Transform picker;
+        picker.position = new Vector2(-5.6f, 0);
+        PlayerPrefs.SetString(avatarKey, "Male");
+        PlayerPrefs.GetString(avatarKey);
+    }
 
-        public void chooseMale()
-        {
-            picker.position = new Vector2(-5.6f, 0);
-        }
-
-        public void chooseFemale()
-        {
-            picker.position = new Vector2(5.5f, 0);
-        }
+    public void chooseFemale()
+    {
+        picker.position = new Vector2(5.5f, 0);
+        PlayerPrefs.SetString(avatarKey, "Female");
+        PlayerPrefs.GetString(avatarKey);
     }
 }
---
+++ Multiplayer Portfolio/Assets/Scripts/Main/PlayerAvatarAssign.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerAvatarAssign : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+}
