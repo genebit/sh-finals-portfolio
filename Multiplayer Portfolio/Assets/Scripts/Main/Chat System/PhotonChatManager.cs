90a26f7b1439a1aea0ec8f61d5d934ec0dc58538
genebit <genebitara@gmail.com>
2021-02-22 14:11:05 +0800

    Done with chat system code, and scroll rect
    TODO: Create a message log design

--- Multiplayer Portfolio/Assets/Scripts/Main/Chat System/PhotonChatManager.cs
+++ Multiplayer Portfolio/Assets/Scripts/Main/Chat System/PhotonChatManager.cs
@@ -7,22 +7,23 @@ using System;
 
 public class PhotonChatManager : MonoBehaviour, IChatClientListener
 {   
+    [Header ("Chat")]
     [SerializeField] private TMP_InputField inputMessageField = null;
-    
     [SerializeField] private TextMeshProUGUI messageLogs = null;
-    
+
     private ChatClient chatClient;
-
-    private string room1 = "room1";
-    private string room2 = "room2";
-
+    private string username;
+    private string chatRoom;
+    
     private void Start()
     {
+        username = PlayerPrefs.GetString("PlayerName");
+
         chatClient = new ChatClient(this);
         chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat,
-        PhotonNetwork.AppVersion, new AuthenticationValues(PlayerPrefs.GetString("PlayerName")));
+        PhotonNetwork.AppVersion, new AuthenticationValues(username));
         
-        chatClient.Subscribe( new string[] { room1, room2 } );
+        chatRoom = "chatRoom";
     }
 
     private void Update() 
@@ -35,7 +36,7 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
     {
         if (!string.IsNullOrEmpty(inputMessageField.text)) 
         {
-            chatClient.PublishMessage(room1, inputMessageField.text);
+            chatClient.PublishMessage(chatRoom, inputMessageField.text);
             inputMessageField.text = "";
         }
     }
@@ -43,6 +44,8 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
     public void OnConnected()
     {
         Debug.Log("Connected");
+        chatClient.Subscribe(new string[] { chatRoom });
+        messageLogs.text = $"{username}: Joined the room.";
     }
 
     public void OnDisconnected()
@@ -53,16 +56,35 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
 
     public void OnGetMessages(string channelName, string[] senders, object[] messages)
     {
-        messageLogs.text = messageLogs.text + Environment.NewLine + inputMessageField.text;
+        for (int i = 0; i < messages.Length; i++)
+        {
+            messageLogs.text = messageLogs.text + Environment.NewLine + $"{senders[i]}: {messages[i]}";
+        }
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        Debug.Log("Subscribed");
+        
+        for (int i = 0; i < channels.Length; i++) 
+        {
+            Debug.Log(channels[i]);
+        }
     }
 
     public void OnPrivateMessage(string sender, object message, string channelName)
     {
+        
+    }
 
+    public void OnUnsubscribed(string[] channels)
+    {
+        Debug.Log("Unsubscribed");
     }
 
     public void DebugReturn(DebugLevel level, string message)
     {
+
     }
 
     public void OnChatStateChange(ChatState state)
@@ -71,16 +93,6 @@ public class PhotonChatManager : MonoBehaviour, IChatClientListener
     }
     
     public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
-    {
-
-    }
-
-    public void OnSubscribed(string[] channels, bool[] results)
-    {
-        
-    }
-
-    public void OnUnsubscribed(string[] channels)
     {
 
     }
